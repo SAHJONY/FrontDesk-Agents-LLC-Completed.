ed1ae28b0b9eb6a2ad70de7d8754051e6f848fb9
SAHJONY <sahjonyllc@outlook.com>
2025-11-17 12:46:51 -0600

    Delete app/app directory

--- app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Welcome to FrontDesk Agents LLC</h1>
-      <p>Your site is live and running on Vercel 🚀</p>
-    </main>
-  );
-}
