92dce2a2639f84006430295dd8518e53e07ae753
SAHJONY <sahjonyllc@outlook.com>
2025-12-05 16:09:10 -0600

    Update LanguageProvider.tsx

--- app/components/LanguageProvider.tsx
+++ app/components/LanguageProvider.tsx
@@ -1,32 +1,4 @@
+// app/components/LanguageProvider.tsx
 "use client";
 
-import { createContext, useContext, useState, ReactNode } from "react";
-
-type Language = "en" | "es";
-
-type LanguageContextValue = {
-  lang: Language;
-  setLang: (lang: Language) => void;
-};
-
-const LanguageContext = createContext<LanguageContextValue | undefined>(
-  undefined
-);
-
-export function useLanguage(): LanguageContextValue {
-  const ctx = useContext(LanguageContext);
-  if (!ctx) {
-    throw new Error("useLanguage must be used within a LanguageProvider");
-  }
-  return ctx;
-}
-
-export default function LangProvider({ children }: { children: ReactNode }) {
-  const [lang, setLang] = useState<Language>("en");
-
-  return (
-    <LanguageContext.Provider value={{ lang, setLang }}>
-      {children}
-    </LanguageContext.Provider>
-  );
-}
+export { default as LanguageProvider, useLanguage } from "./LangProvider";
