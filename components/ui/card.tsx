8ffb3369f63adb5cd8738b1416910740f9aa14ed
SAHJONY <sahjonyllc@outlook.com>
2025-12-16 18:52:04 -0600

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,38 +1,5 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
 
-export const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "rounded-2xl border bg-white text-gray-900 shadow-sm",
-      className
-    )}
-    {...props}
-  />
-));
-Card.displayName = "Card";
-
-export const CardHeader = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("p-6 pb-2", className)} {...props} />
-);
-
-export const CardContent = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("p-6 pt-2", className)} {...props} />
-);
-
-export const CardFooter = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("p-6 pt-0", className)} {...props} />
-);
+export function Card({ children }: { children: ReactNode }) {
+  return <div className="rounded-lg border p-4">{children}</div>;
+}
