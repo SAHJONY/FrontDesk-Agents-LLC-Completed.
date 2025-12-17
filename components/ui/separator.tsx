d49cacd0b96a0067ef0d65885d42b00bb900eb76
SAHJONY <sahjonyllc@outlook.com>
2025-12-16 18:24:15 -0600

    Update separator.tsx

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,18 +1,3 @@
-// ./components/ui/separator.tsx (Modificación de la destructuración)
-'use client';
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
-import { cn } from "@/lib/utils" 
-
-export const Separator = ({ className = "", orientation = 'horizontal', decorative = true, ...props }) => (
-//                                    ^^^^^^^^^  Asegurarse de que className siempre tiene un valor por defecto (string vacío)
-  <SeparatorPrimitive.Root
-    orientation={orientation}
-    decorative={decorative}
-    className={cn(
-      "shrink-0 bg-gray-200", 
-      orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-      className
-    )}
-    {...props}
-  />
-);
+export function Separator() {
+  return <hr className="my-4 border-gray-200" />;
+}
