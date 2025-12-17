6dd55a1853ce32d7f427c8a5e6c9aff1b3513c7b
SAHJONY <sahjonyllc@outlook.com>
2025-12-16 18:53:36 -0600

    Update switch.tsx

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -1,39 +1,10 @@
 "use client";
 
-import * as React from "react";
-import { cn } from "@/lib/utils";
-
-interface SwitchProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  checked?: boolean;
-  onCheckedChange?: (checked: boolean) => void;
-}
-
-export const Switch = ({
-  checked = false,
-  onCheckedChange,
-  className,
-  ...props
-}: SwitchProps) => {
+export function Switch() {
   return (
-    <button
-      type="button"
-      role="switch"
-      aria-checked={checked}
-      onClick={() => onCheckedChange?.(!checked)}
-      className={cn(
-        "relative inline-flex h-6 w-11 items-center rounded-full transition-colors",
-        checked ? "bg-black" : "bg-gray-300",
-        className
-      )}
-      {...props}
-    >
-      <span
-        className={cn(
-          "inline-block h-5 w-5 transform rounded-full bg-white transition-transform",
-          checked ? "translate-x-5" : "translate-x-1"
-        )}
-      />
-    </button>
+    <input
+      type="checkbox"
+      className="h-4 w-4 cursor-pointer"
+    />
   );
-};
+}
