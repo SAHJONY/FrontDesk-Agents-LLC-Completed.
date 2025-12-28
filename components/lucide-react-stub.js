4b7bbfa2d9e3cb139545b454deee53b1db9d036d
SAHJONY <sahjonyllc@outlook.com>
2025-12-28 17:10:54 -0600

    Delete components directory

--- components/lucide-react-stub.js
+++
@@ -1,8 +0,0 @@
-// components/lucide-react-stub.js
-// This is a stub to replace lucide-react imports
-
-export const Menu = () => <div className="w-6 h-6 bg-gray-300"></div>;
-export const X = () => <div className="w-6 h-6 bg-gray-300"></div>;
-export const Search = () => <div className="w-6 h-6 bg-gray-300"></div>;
-export const Bell = () => <div className="w-6 h-6 bg-gray-300"></div>;
-export const ChevronDown = () => <div className="w-6 h-6 bg-gray-300"></div>;
