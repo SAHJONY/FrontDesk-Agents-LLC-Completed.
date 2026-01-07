94ed8518eff92d1023ca985b1c96f9527803d09f
Juan Gonzalez <frontdeskllc@outlook.com>
2026-01-07 08:05:13 -0500

    🔧 Add Navigation component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
-"use client";
+'use client';
 
 import React from 'react';
+import Navigation from '@/components/Navigation';
 import { HeroSectionEnhanced } from '@/components/landing/HeroSectionEnhanced';
 import { FeaturesSection, TestimonialsSection, CTASection } from '@/components/landing';
 
 export default function LandingPage() {
   return (
-    <main className="bg-black text-white">
-      <HeroSectionEnhanced />
-      <FeaturesSection />
-      <TestimonialsSection />
-      <CTASection />
-    </main>
+    <>
+      <Navigation />
+      <main className="bg-black text-white">
+        <HeroSectionEnhanced />
+        <FeaturesSection />
+        <TestimonialsSection />
+        <CTASection />
+      </main>
+    </>
   );
 }
-// Force rebuild Wed Jan  7 00:14:39 EST 2026
