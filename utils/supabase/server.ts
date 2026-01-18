652933a8487478e7a1cdfcaa1b329624dbc8a248
SAHJONY <sahjonyllc@outlook.com>
2026-01-18 16:58:10 -0600

    Update server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -3,6 +3,9 @@ import { cookies } from 'next/headers'
 
 export async function createClient() {
   const cookieStore = await cookies()
+  
+  // Detect if the Super Admin is currently in 'Impersonation Mode'
+  const impersonateId = cookieStore.get('impersonate_id')?.value
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -19,11 +22,19 @@ export async function createClient() {
             )
           } catch {
             // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            // This can be ignored if you have middleware refreshing user sessions.
           }
         },
       },
+      // --- IMPERSONATION OVERRIDE ---
+      // We inject a custom header if we are impersonating. 
+      // You can use this header in your Postgres RLS policies 
+      // or to filter data in your server actions.
+      global: impersonateId ? {
+        headers: {
+          'x-impersonate-user-id': impersonateId,
+        },
+      } : undefined,
     }
   )
 }
