00f4438c92ee2273e11b7beb226c38848a93fabe
SAHJONY <sahjonyllc@outlook.com>
2026-01-19 12:57:38 -0600

    Create sms.ts

---
+++ lib/sms.ts
@@ -0,0 +1,20 @@
+export async function sendFollowUpSMS(to: string, message: string) {
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const fromNumber = process.env.TWILIO_PHONE_NUMBER;
+
+  const res = await fetch(`https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`, {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + btoa(`${accountSid}:${authToken}`),
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      To: to,
+      From: fromNumber!,
+      Body: message,
+    }),
+  });
+
+  return res.ok;
+}
